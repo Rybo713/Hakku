70eedd4c5fcf6f06b22f9298fd648c383c71d0e1
rybo713 <ryanwong713@gmail.com>
2019-04-07 17:11:51 -0700

    Shell script for settings permissions with one command

---
+++ perm.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Sets pemissions for the Hakku2 Tool
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Ryan Wong
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+LGREEN='\033[1;32m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+NC='\033[0m'
+LYELLOW='\033[1;33m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+if [[ $EUID -ne 0 ]]; then
+  printf "${RED}${bold}[ERROR] ${NC}${normal}This script must be run as root\n"
+  exit 1
+elif [[ $EUID -ne 1 ]]; then
+  echo ""
+fi
+
+printf "${GREEN}${bold}[INFO] ${NC}${normal}Settings Permissions...\n"
+sudo chmod +x ./Hakku2.sh
+sudo chmod +x ./functions.sh
+sudo chmod +x ./settings_f.sh
+sudo chmod +x ./update_f.sh
+sudo chmod +x ./tweaks_f.sh
+printf "${GREEN}${bold}[INFO] ${NC}${normal}Finished settings permissions!\n"
