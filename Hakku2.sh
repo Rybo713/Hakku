78ff06d22193eeb3efaf742dad641d1aa572c95c
Ryan Wong <ryanwong713@gmail.com>
2021-03-26 14:14:53 -0700

    Updated copyright info

--- Hakku2.sh
+++ Hakku2.sh
@@ -6,7 +6,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2021 Ryan Wong
+# Copyright (c) 2019-2021 Ryan Wong
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
