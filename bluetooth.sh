42a342f8041d4ea3dac307a98e2815cd2691c815
Ryan Wong <ryanwong713@gmail.com>
2021-03-26 14:14:46 -0700

    Removed bluetooth.sh

--- bluetooth.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo kextunload -b com.apple.iokit.BroadcomBluetooth20703USBTransport
-sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-sudo kextload -b com.apple.iokit.BroadcomBluetooth20703USBTransport
